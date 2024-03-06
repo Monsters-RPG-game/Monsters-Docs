58acfb73a6d3b035d1f5bc635976fae35d9b0b35
HurasAdam <huradam@wp.pl>
2024-03-06 18:03:05 +0100

    Feat: Added Icon conponent, logo and icon links to TopNavbar component.

--- src/types/navbar.d.ts
+++ src/types/navbar.d.ts
@@ -3,9 +3,24 @@ export interface ITopNavbarProps {
   handleMobileMenu: () => void;
 }
 
+
+export interface IIconProps {
+  icon: string;
+  link: string;
+  color: string;
+
+}
+
 export interface INavLink {
   route: string;
   label: string;
+}
+
+export interface IIconLink {
+  link: string;
+  icon: string;
+  color: string;
+  hover: string;
 }
 
 export interface ISidebar {
