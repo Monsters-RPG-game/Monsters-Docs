0e297ccb20438c3e087ea60cad5093eb058e2f5b
Jakub <kiszczyckuba20@wp.pl>
2024-03-08 16:51:40 +0100

    Fix: Added fix for incorrect url handler

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import type { IIconProps } from '../types';
 
 const Icon: React.FC<IIconProps> = ({ icon, link, color }) => {
-  const navigate = useNavigate();
-
   return (
     // eslint-disable-next-line jsx-a11y/control-has-associated-label
     <a href={link} target="_blank" rel="noreferrer">
-      <div className="cursor-pointer" onClick={() => navigate(link)}>
+      <div className="cursor-pointer">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="23"
